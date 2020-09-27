83ba58e3c068e0ab244cc14efe0164d0a02d3575
preeti121207 <69007432+preeti121207@users.noreply.github.com>
2020-09-27 11:03:23 -0700

    Add files via upload

---
+++ dustbin.js
@@ -0,0 +1,25 @@
+class Bin {
+    constructor(x,y,width,height) {
+      var options = {
+        isStatic: true,
+        friction: 6.5
+      }
+      this.body = Bodies.rectangle(x,y,width,height/4,options);
+      this.width = width;
+      this.height = height;
+       
+      this.image = loadImage("dustbingreen.png");
+      
+
+      World.add(world, this.body);
+    }
+    display(){
+      var pos =this.body.position;
+      imageMode(CENTER);
+      image(this.image, pos.x+20,pos.y-30, 100,100);
+        //rectMode(CENTER);
+        //fill("red");
+        //rect(pos.x,pos.y,this.width,this.height);
+      
+    }
+  }
